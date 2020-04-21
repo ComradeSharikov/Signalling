d7f2cf907f07a2a7187ee659e9c0fac22247635e
ComradeSharikov <62386083+ComradeSharikov@users.noreply.github.com>
2020-04-21 21:13:37 +0300

    Enemy Spawn

---
+++ SignallingProject/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private Transform _spawnPoints;
+    [SerializeField] private Enemy _enemy;
+
+    private Transform[] _points;
+    private int _currentPoint;
+
+    private void Start()
+    {
+        _points = new Transform[_spawnPoints.childCount];
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = _spawnPoints.GetChild(i);
+        }
+
+        StartCoroutine(SpawnEnemy());       
+    }
+   
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            Transform target = _points[_currentPoint];
+            Instantiate(_enemy, target.position, Quaternion.identity);
+            _currentPoint++;
+
+            if (_currentPoint >= _points.Length)
+            {
+                _currentPoint = 0;
+            }
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
