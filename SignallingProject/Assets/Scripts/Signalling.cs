d67d42da73c7a7c6f1fa3a20bd150939bea9edd9
ComradeSharikov <62386083+ComradeSharikov@users.noreply.github.com>
2020-04-22 17:01:41 +0300

    Add coin, wallet, enemy

---
+++ SignallingProject/Assets/Scripts/Signalling.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource), typeof(Door))]
+public class Signalling : MonoBehaviour
+{
+    [SerializeField] private Door _door;
+
+    private AudioSource _audioSource;
+    private Coroutine _setAudioVolumeJob;
+    private float _maxVolume = 1f;
+    private float _minVolume = 0.15f; 
+
+    private void OnEnable()
+    {
+        _door.Reached += OnDoorReached;
+    }
+
+    private void OnDisable()
+    {
+        _door.Reached -= OnDoorReached;
+    }
+
+    private void Start()
+    {
+        _door = GetComponent<Door>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnDoorReached()
+    {
+        if (_door.IsReached)
+        {
+            _setAudioVolumeJob = StartCoroutine(SetAudioVolume());
+        }
+        else
+        {
+            _audioSource.Stop();
+            StopCoroutine(_setAudioVolumeJob);
+        }
+    }
+
+    private IEnumerator SetAudioVolume()
+    {
+        _audioSource.volume = _maxVolume;
+        bool isFading = true;
+
+        while (true)
+        {
+            if (isFading)
+            {
+                _audioSource.volume -= Time.deltaTime / 2f;
+                if (_audioSource.volume <= _minVolume)
+                {
+                    isFading = false;
+                }
+            }
+            else
+            {
+                _audioSource.volume += Time.deltaTime / 2f;
+                if (_audioSource.volume >= _maxVolume)
+                {
+                    isFading = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ SignallingProject/Assets/Scripts/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wallet : MonoBehaviour
+{
+    private int _count;
+    [SerializeField] Text _coinCountText;
+
+    public void AddCoin()
+    {
+        _count++;
+        _coinCountText.text = _count.ToString();
+    }
+}
